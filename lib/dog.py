825928fda24dcbc8638081019b08f8fd5666c48c
Quincy Michael <michaelkirima865@gmail.com>
2024-11-28 12:15:53 +0300

    Init self

--- lib/dog.py
+++ lib/dog.py
@@ -1,4 +1,25 @@
 #!/usr/bin/env python3
 
 class Dog:
-    pass
+    def __init__(self, name, breed="Mutt"):
+        self.name = name     
+        self.breed = breed 
+
+    def bark(self):
+        print("Woof!") 
+    
+    def sit(self):
+        print("The dog is sitting.") 
+
+fido = Dog("Fido", "Beagle")  
+print(fido.name) 
+print(fido.breed)  
+fido.bark() 
+fido.sit() 
+
+buddy = Dog("Buddy")
+print(buddy.name)
+print(buddy.breed) 
+buddy.bark() 
+buddy.sit()  
+
--- lib/person.py
+++ lib/person.py
@@ -1,4 +1,18 @@
 #!/usr/bin/env python3
 
 class Person:
-    pass
+    def __init__(self, name):
+        self.name = name 
+    
+    def talk(self):
+        print("Hello World!")
+    
+    def walk(self):
+        print("The person is walking.")
+
+
+john = Person("John") 
+print(john.name)  
+john.talk() 
+john.walk() 
+
